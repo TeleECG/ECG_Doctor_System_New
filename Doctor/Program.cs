b113e1e3b72da8f1b5328a4bc09edf3b3fe1ba67
carolinenissen <201806156@uni.au.dk>
2020-05-05 15:06:17 +0200

    ....

--- Doctor/Program.cs
+++ Doctor/Program.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Domain;
 
 namespace Doctor
 {
     public class Program : IForm
     {
         private Get_ECG_Controller _getEcgController;
+        private ECG_Meassure _ecgMeassure;
 
-        public Program(Get_ECG_Controller getEcgController)
+        public Program(Get_ECG_Controller getEcgController, ECG_Meassure ecgMeassure)
         {
             _getEcgController = getEcgController;
+            _ecgMeassure = ecgMeassure;
         }
 
         [STAThread]
@@ -20,7 +23,7 @@ namespace Doctor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Medical_Display(_getEcgController,));
+            Application.Run(new Medical_Display(_getEcgController,_ecgMeassure));
         }
     }
 }
