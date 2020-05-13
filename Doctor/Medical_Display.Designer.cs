f9886c37877e90dbefa331db7a07b524709b0562
carolinenissen <201806156@uni.au.dk>
2020-05-13 15:11:47 +0200

    graf

--- Doctor/Medical_Display.Designer.cs
+++ Doctor/Medical_Display.Designer.cs
@@ -72,9 +72,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(24, 31);
+            this.label1.Location = new System.Drawing.Point(27, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 23);
+            this.label1.Size = new System.Drawing.Size(140, 27);
             this.label1.TabIndex = 0;
             this.label1.Text = "CPR-nummer:";
             // 
@@ -83,30 +83,30 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(24, 81);
+            this.label2.Location = new System.Drawing.Point(27, 101);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 23);
+            this.label2.Size = new System.Drawing.Size(161, 27);
             this.label2.TabIndex = 1;
             this.label2.Text = "Dato for måling:";
             // 
             // CPRTB
             // 
             this.CPRTB.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CPRTB.Location = new System.Drawing.Point(192, 31);
+            this.CPRTB.Location = new System.Drawing.Point(216, 39);
             this.CPRTB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.CPRTB.Name = "CPRTB";
-            this.CPRTB.Size = new System.Drawing.Size(207, 30);
+            this.CPRTB.Size = new System.Drawing.Size(232, 34);
             this.CPRTB.TabIndex = 2;
             // 
             // DateLB
             // 
             this.DateLB.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.DateLB.FormattingEnabled = true;
-            this.DateLB.ItemHeight = 22;
-            this.DateLB.Location = new System.Drawing.Point(192, 91);
+            this.DateLB.ItemHeight = 27;
+            this.DateLB.Location = new System.Drawing.Point(216, 114);
             this.DateLB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DateLB.Name = "DateLB";
-            this.DateLB.Size = new System.Drawing.Size(207, 48);
+            this.DateLB.Size = new System.Drawing.Size(232, 58);
             this.DateLB.TabIndex = 3;
             // 
             // SearchB
@@ -114,10 +114,10 @@
             this.SearchB.BackColor = System.Drawing.Color.Green;
             this.SearchB.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SearchB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.SearchB.Location = new System.Drawing.Point(432, 26);
+            this.SearchB.Location = new System.Drawing.Point(486, 32);
             this.SearchB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.SearchB.Name = "SearchB";
-            this.SearchB.Size = new System.Drawing.Size(102, 37);
+            this.SearchB.Size = new System.Drawing.Size(115, 46);
             this.SearchB.TabIndex = 4;
             this.SearchB.Text = "Søg";
             this.SearchB.UseVisualStyleBackColor = false;
@@ -128,10 +128,10 @@
             this.DateB.BackColor = System.Drawing.Color.Green;
             this.DateB.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.DateB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.DateB.Location = new System.Drawing.Point(432, 104);
+            this.DateB.Location = new System.Drawing.Point(486, 130);
             this.DateB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DateB.Name = "DateB";
-            this.DateB.Size = new System.Drawing.Size(102, 41);
+            this.DateB.Size = new System.Drawing.Size(115, 51);
             this.DateB.TabIndex = 5;
             this.DateB.Text = "Vis måling";
             this.DateB.UseVisualStyleBackColor = false;
@@ -142,9 +142,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(613, 26);
+            this.label3.Location = new System.Drawing.Point(690, 32);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 23);
+            this.label3.Size = new System.Drawing.Size(65, 27);
             this.label3.TabIndex = 6;
             this.label3.Text = "Navn:";
             // 
@@ -153,9 +153,9 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Location = new System.Drawing.Point(613, 81);
+            this.label4.Location = new System.Drawing.Point(690, 101);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(80, 23);
+            this.label4.Size = new System.Drawing.Size(91, 27);
             this.label4.TabIndex = 7;
             this.label4.Text = "Adresse:";
             // 
@@ -164,12 +164,12 @@
             this.NameTB.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.NameTB.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NameTB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.NameTB.Location = new System.Drawing.Point(707, 23);
+            this.NameTB.Location = new System.Drawing.Point(795, 29);
             this.NameTB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.NameTB.Multiline = true;
             this.NameTB.Name = "NameTB";
             this.NameTB.ReadOnly = true;
-            this.NameTB.Size = new System.Drawing.Size(222, 32);
+            this.NameTB.Size = new System.Drawing.Size(249, 39);
             this.NameTB.TabIndex = 8;
             // 
             // AdressTB
@@ -177,12 +177,12 @@
             this.AdressTB.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.AdressTB.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.AdressTB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.AdressTB.Location = new System.Drawing.Point(707, 81);
+            this.AdressTB.Location = new System.Drawing.Point(795, 101);
             this.AdressTB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.AdressTB.Multiline = true;
             this.AdressTB.Name = "AdressTB";
             this.AdressTB.ReadOnly = true;
-            this.AdressTB.Size = new System.Drawing.Size(222, 31);
+            this.AdressTB.Size = new System.Drawing.Size(249, 38);
             this.AdressTB.TabIndex = 9;
             // 
             // ECG1Chart
@@ -197,7 +197,7 @@
             this.ECG1Chart.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.ECG1Chart.Legends.Add(legend1);
-            this.ECG1Chart.Location = new System.Drawing.Point(57, 169);
+            this.ECG1Chart.Location = new System.Drawing.Point(64, 221);
             this.ECG1Chart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ECG1Chart.Name = "ECG1Chart";
             series1.ChartArea = "ChartArea1";
@@ -206,7 +206,7 @@
             series1.Legend = "Legend1";
             series1.Name = "Måling 1";
             this.ECG1Chart.Series.Add(series1);
-            this.ECG1Chart.Size = new System.Drawing.Size(989, 167);
+            this.ECG1Chart.Size = new System.Drawing.Size(1217, 311);
             this.ECG1Chart.TabIndex = 10;
             this.ECG1Chart.Text = "chart1";
             this.ECG1Chart.Visible = false;
@@ -223,7 +223,7 @@
             this.ECG2Chart.ChartAreas.Add(chartArea2);
             legend2.Name = "Legend1";
             this.ECG2Chart.Legends.Add(legend2);
-            this.ECG2Chart.Location = new System.Drawing.Point(57, 340);
+            this.ECG2Chart.Location = new System.Drawing.Point(64, 559);
             this.ECG2Chart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ECG2Chart.Name = "ECG2Chart";
             series2.ChartArea = "ChartArea1";
@@ -232,7 +232,7 @@
             series2.Legend = "Legend1";
             series2.Name = "Måling 2";
             this.ECG2Chart.Series.Add(series2);
-            this.ECG2Chart.Size = new System.Drawing.Size(989, 161);
+            this.ECG2Chart.Size = new System.Drawing.Size(1217, 305);
             this.ECG2Chart.TabIndex = 11;
             this.ECG2Chart.Text = "chart1";
             this.ECG2Chart.Visible = false;
@@ -249,7 +249,7 @@
             this.ECG3Chart.ChartAreas.Add(chartArea3);
             legend3.Name = "Legend1";
             this.ECG3Chart.Legends.Add(legend3);
-            this.ECG3Chart.Location = new System.Drawing.Point(58, 505);
+            this.ECG3Chart.Location = new System.Drawing.Point(64, 890);
             this.ECG3Chart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ECG3Chart.Name = "ECG3Chart";
             series3.ChartArea = "ChartArea1";
@@ -258,7 +258,7 @@
             series3.Legend = "Legend1";
             series3.Name = "Måling 3";
             this.ECG3Chart.Series.Add(series3);
-            this.ECG3Chart.Size = new System.Drawing.Size(988, 163);
+            this.ECG3Chart.Size = new System.Drawing.Size(1217, 305);
             this.ECG3Chart.TabIndex = 12;
             this.ECG3Chart.Text = "chart1";
             this.ECG3Chart.Visible = false;
@@ -268,9 +268,9 @@
             this.Puls1.AutoSize = true;
             this.Puls1.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Puls1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Puls1.Location = new System.Drawing.Point(1073, 169);
+            this.Puls1.Location = new System.Drawing.Point(1311, 315);
             this.Puls1.Name = "Puls1";
-            this.Puls1.Size = new System.Drawing.Size(48, 23);
+            this.Puls1.Size = new System.Drawing.Size(56, 27);
             this.Puls1.TabIndex = 13;
             this.Puls1.Text = "Puls:";
             // 
@@ -279,9 +279,9 @@
             this.HRV1.AutoSize = true;
             this.HRV1.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.HRV1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.HRV1.Location = new System.Drawing.Point(1073, 215);
+            this.HRV1.Location = new System.Drawing.Point(1311, 373);
             this.HRV1.Name = "HRV1";
-            this.HRV1.Size = new System.Drawing.Size(48, 23);
+            this.HRV1.Size = new System.Drawing.Size(56, 27);
             this.HRV1.TabIndex = 14;
             this.HRV1.Text = "HRV:";
             // 
@@ -290,9 +290,9 @@
             this.Puls2.AutoSize = true;
             this.Puls2.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Puls2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Puls2.Location = new System.Drawing.Point(1073, 350);
+            this.Puls2.Location = new System.Drawing.Point(1311, 651);
             this.Puls2.Name = "Puls2";
-            this.Puls2.Size = new System.Drawing.Size(48, 23);
+            this.Puls2.Size = new System.Drawing.Size(56, 27);
             this.Puls2.TabIndex = 15;
             this.Puls2.Text = "Puls:";
             // 
@@ -301,9 +301,9 @@
             this.Hrv2.AutoSize = true;
             this.Hrv2.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Hrv2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Hrv2.Location = new System.Drawing.Point(1073, 397);
+            this.Hrv2.Location = new System.Drawing.Point(1311, 709);
             this.Hrv2.Name = "Hrv2";
-            this.Hrv2.Size = new System.Drawing.Size(48, 23);
+            this.Hrv2.Size = new System.Drawing.Size(56, 27);
             this.Hrv2.TabIndex = 16;
             this.Hrv2.Text = "HRV:";
             // 
@@ -312,9 +312,9 @@
             this.Hrv3.AutoSize = true;
             this.Hrv3.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Hrv3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Hrv3.Location = new System.Drawing.Point(1073, 551);
+            this.Hrv3.Location = new System.Drawing.Point(1311, 1021);
             this.Hrv3.Name = "Hrv3";
-            this.Hrv3.Size = new System.Drawing.Size(48, 23);
+            this.Hrv3.Size = new System.Drawing.Size(56, 27);
             this.Hrv3.TabIndex = 18;
             this.Hrv3.Text = "HRV:";
             // 
@@ -323,9 +323,9 @@
             this.Puls3.AutoSize = true;
             this.Puls3.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Puls3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Puls3.Location = new System.Drawing.Point(1073, 505);
+            this.Puls3.Location = new System.Drawing.Point(1311, 963);
             this.Puls3.Name = "Puls3";
-            this.Puls3.Size = new System.Drawing.Size(48, 23);
+            this.Puls3.Size = new System.Drawing.Size(56, 27);
             this.Puls3.TabIndex = 17;
             this.Puls3.Text = "Puls:";
             // 
@@ -334,9 +334,9 @@
             this.puls1l.AutoSize = true;
             this.puls1l.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.puls1l.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.puls1l.Location = new System.Drawing.Point(1164, 164);
+            this.puls1l.Location = new System.Drawing.Point(1414, 309);
             this.puls1l.Name = "puls1l";
-            this.puls1l.Size = new System.Drawing.Size(20, 29);
+            this.puls1l.Size = new System.Drawing.Size(24, 35);
             this.puls1l.TabIndex = 19;
             this.puls1l.Text = "-";
             // 
@@ -345,9 +345,9 @@
             this.hrv1l.AutoSize = true;
             this.hrv1l.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hrv1l.ForeColor = System.Drawing.Color.Gold;
-            this.hrv1l.Location = new System.Drawing.Point(1164, 215);
+            this.hrv1l.Location = new System.Drawing.Point(1414, 373);
             this.hrv1l.Name = "hrv1l";
-            this.hrv1l.Size = new System.Drawing.Size(20, 29);
+            this.hrv1l.Size = new System.Drawing.Size(24, 35);
             this.hrv1l.TabIndex = 20;
             this.hrv1l.Text = "-";
             // 
@@ -356,9 +356,9 @@
             this.hrv2l.AutoSize = true;
             this.hrv2l.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hrv2l.ForeColor = System.Drawing.Color.Gold;
-            this.hrv2l.Location = new System.Drawing.Point(1165, 397);
+            this.hrv2l.Location = new System.Drawing.Point(1415, 709);
             this.hrv2l.Name = "hrv2l";
-            this.hrv2l.Size = new System.Drawing.Size(20, 29);
+            this.hrv2l.Size = new System.Drawing.Size(24, 35);
             this.hrv2l.TabIndex = 22;
             this.hrv2l.Text = "-";
             // 
@@ -367,9 +367,9 @@
             this.puls2l.AutoSize = true;
             this.puls2l.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.puls2l.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.puls2l.Location = new System.Drawing.Point(1165, 350);
+            this.puls2l.Location = new System.Drawing.Point(1415, 651);
             this.puls2l.Name = "puls2l";
-            this.puls2l.Size = new System.Drawing.Size(20, 29);
+            this.puls2l.Size = new System.Drawing.Size(24, 35);
             this.puls2l.TabIndex = 21;
             this.puls2l.Text = "-";
             // 
@@ -378,9 +378,9 @@
             this.hrv3l.AutoSize = true;
             this.hrv3l.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hrv3l.ForeColor = System.Drawing.Color.Gold;
-            this.hrv3l.Location = new System.Drawing.Point(1164, 546);
+            this.hrv3l.Location = new System.Drawing.Point(1414, 1014);
             this.hrv3l.Name = "hrv3l";
-            this.hrv3l.Size = new System.Drawing.Size(20, 29);
+            this.hrv3l.Size = new System.Drawing.Size(24, 35);
             this.hrv3l.TabIndex = 24;
             this.hrv3l.Text = "-";
             // 
@@ -389,18 +389,18 @@
             this.puls3l.AutoSize = true;
             this.puls3l.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.puls3l.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.puls3l.Location = new System.Drawing.Point(1165, 500);
+            this.puls3l.Location = new System.Drawing.Point(1415, 957);
             this.puls3l.Name = "puls3l";
-            this.puls3l.Size = new System.Drawing.Size(20, 29);
+            this.puls3l.Size = new System.Drawing.Size(24, 35);
             this.puls3l.TabIndex = 23;
             this.puls3l.Text = "-";
             // 
             // Medical_Display
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(1364, 699);
+            this.ClientSize = new System.Drawing.Size(1784, 1230);
             this.Controls.Add(this.hrv3l);
             this.Controls.Add(this.puls3l);
             this.Controls.Add(this.hrv2l);
